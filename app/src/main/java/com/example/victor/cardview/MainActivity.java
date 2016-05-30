d2d261e5e4c58b49cbd0baedfd77847ab9d03285
victsomie <victsomie@yahoo.com>
2016-05-30 17:22:07 +0300

    added file for the calculator

---
+++ app/src/main/java/com/example/victor/cardview/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.victor.cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
